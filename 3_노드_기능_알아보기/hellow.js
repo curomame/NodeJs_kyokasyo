adc728b7a759b1b37f672ff81e93a10ea0367b87
HyangRim <curomame@naver.com>
2022-06-03 23:00:50 +0900

    study : 3장 진행중

---
+++ 3_노드_기능_알아보기/hellow.js
@@ -0,0 +1,16 @@
+function helloWorld(){
+  console.log('Hello World');
+  helloNode();
+}
+
+function helloNode(){
+  console.log('Hello Node');
+}
+
+helloWorld();
+
+//노드는 코드를 모듈로 만들수 있음
+//모듈 - 특정한 기능을 하는 함수나 변수들의 집합
+//모듈은 자체로도 하나의 프로그램이면서 다른 프로그램의 부품으로도 사용
+// 여러 프로그램에서 모듈을 재사용 가능
+// 보통 파일 하나가 모듈 하나
---
+++ 3_노드_기능_알아보기/var.js
@@ -0,0 +1,14 @@
+// const odd = 'odd';
+// const even = 'even'
+
+// module.exports = {
+//   odd,
+//   even,
+// };
+
+
+exports.odd = 'odd'
+exports.even = 'even';
+
+//this
+//최상위 스코프의 this는 module.exports 또는 exports 객체를 가리킴, 함수 선언문 내부의 this는 global객체를 가리킴
