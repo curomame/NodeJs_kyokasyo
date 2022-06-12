bd59d8f57a43d7dbeaebdfd3843ea9c6e5f2f908
HyangRim <curomame@naver.com>
2022-06-12 21:46:56 +0900

    study : 8장 끝

--- 8_MONGODB/app.js
+++ 8_MONGODB/app.js
@@ -5,6 +5,10 @@ const nunjucks = require('nunjucks');
 
 const connect = require('./schemas');
 
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
 const app = express();
 app.set('port',process.env.PORT || 3002);
 app.set('view engine','html');
@@ -20,6 +24,10 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+app.use('/',indexRouter);
+app.use('/users',usersRouter);
+app.use('/comments',commentsRouter);
+
 app.use((req,res,next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
   error.status = 404;
@@ -34,5 +42,5 @@ app.use((err,req,res,next) => {
 })
 
 app.listen(app.get('port'), () => {
-  console.log(app.get(port));
+  console.log(app.get('port'));
 });
